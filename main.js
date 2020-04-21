8384dc2849fdec9eb1132eef88656b1fae9139a8
Todd Shinto <todd_thirty_one@yahoo.com>
2020-04-21 16:45:27 -0700

    added ability to match

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 var gameCards = document.getElementById('gameCards');
+var firstCardClicked;
+var secondCardClicked;
+var firstCardClasses;
+var secondCardClasses;
+
 gameCards.addEventListener('click', handleClick);
 
 function handleClick(event) {
@@ -6,4 +11,27 @@ function handleClick(event) {
     return;
   }
   event.target.className += ' hidden';
+  if (!firstCardClicked) {
+    firstCardClicked = event.target;
+    firstCardClasses = firstCardClicked.previousElementSibling.className;
+  } else {
+    secondCardClicked = event.target;
+    secondCardClasses = secondCardClicked.previousElementSibling.className;
+    gameCards.removeEventListener('click', handleClick);
+    if (firstCardClasses === secondCardClasses) {
+      console.log('match');
+      gameCards.addEventListener('click', handleClick);
+      firstCardClicked = null;
+      secondCardClicked = null;
+    } else {
+      console.log('no match');
+      setTimeout(function() {
+        firstCardClicked.classList.remove('hidden');
+        secondCardClicked.classList.remove('hidden');
+        gameCards.addEventListener('click', handleClick);
+        firstCardClicked = null;
+        secondCardClicked = null;
+      }, 1500);
+    }
+  }
 }
