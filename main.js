9cd87f50397a471dc42ab28ef0e9f09bb82daecb
Todd Shinto <todd_thirty_one@yahoo.com>
2020-04-24 01:11:30 -0700

    improved win screen & changed play again functionality

--- main.js
+++ main.js
@@ -7,8 +7,16 @@ var maxMatches = 9;
 var matches = 0;
 var attempts = 0;
 var gamesPlayed = 0;
-var hiddenCards;
-var playAgain = document.getElementById('play-again');
+var hiddenCards = null;
+var hiddenClass = null;
+var hiddenIndex = [];
+var storeHiddenClasses  = null;
+var container = document.querySelector('#container');
+var congrats = document.getElementById('congrats');
+var winScreen = document.querySelector("#win-screen");
+var spongeAgain = document.getElementById('sponge-again');
+var dinoAgain = document.getElementById('dino-again');
+var thomasAgain = document.getElementById('thomas-again');
 var youWin = document.querySelector('.you-win');
 var spongebobButton = document.getElementById('spongebob');
 var thomasButton = document.getElementById('thomas');
@@ -38,7 +46,9 @@ var list = ['css-logo',
 
 createCards();
 gameCards.addEventListener('click', handleClick);
-playAgain.addEventListener('click', resetGame);
+spongeAgain.addEventListener('click', resetGame);
+thomasAgain.addEventListener('click', resetGame);
+dinoAgain.addEventListener('click', resetGame);
 spongebobButton.addEventListener('click', spongebobTheme);
 thomasButton.addEventListener('click', thomasTheme);
 dinoButton.addEventListener('click', dinoTheme);
@@ -47,23 +57,32 @@ smallSpongebobButton.addEventListener('click', spongebobTheme);
 smallThomasButton.addEventListener('click', thomasTheme);
 
 function spongebobTheme() {
-  if (startScreen.className != 'hidden') {
-    startScreen.className = 'hidden';
+  if (startScreen.className != 'hidden') { //checks if start screen is hidden
+    startScreen.className = 'hidden'; //if start screen is not hidden, sets to hidden
   }
-  document.querySelector('#title').className = 'spongebob-title';
-  let statsList = document.querySelectorAll('.stats');
+  getHiddenIndex(); //stores matched cards to presere during theme change
+  document.querySelector('#title').className = 'spongebob-title'; //changes title format
+  congrats.className += ' spongebob-title'; //changes congrats format
+  let statsList = document.querySelectorAll('.stats'); //changes stat boxes formatting through setting classname
   for (let i = 0; i < statsList.length; i++) {
     statsList[i].className = 'stats spongebob-stats';
   }
-  document.querySelector('#misc-img').className = 'misc-img spongebob-img';
-  let cardList = document.querySelectorAll('.card-back');
+  document.querySelector('#misc-img').className = 'misc-img spongebob-img'; //changes bottom left image
+  let cardList = document.querySelectorAll('.card-back'); //changes card backs
   for (let x = 0; x < cardList.length; x++) {
     cardList[x].className = 'card-back spongebob-back';
   }
-  document.querySelector('#bg').className = 'spongebob-bg';
+  document.querySelector('#bg').className = 'spongebob-bg'; //changes background
+  if (winScreen.classList.value.includes('hidden')) { //changes win screen styling without changing hidden status
+    winScreen.className = 'you-win spongebob-win hidden';
+  } else {
+    winScreen.className = 'you-win spongebob-win';
+  }
+  restoreHidden(); //restores matched cards during theme change
 }
 
 function thomasTheme() {
+  getHiddenIndex();
   if (startScreen.className != 'hidden') {
     startScreen.className = 'hidden';
   }
@@ -78,9 +97,16 @@ function thomasTheme() {
     cardList[x].className = 'card-back thomas-back';
   }
   document.querySelector('#bg').className = 'thomas-bg';
+  if (winScreen.classList.value.includes('hidden')) {
+    winScreen.className = 'you-win thomas-win hidden';
+  } else {
+    winScreen.className = 'you-win thomas-win';
+  }
+  restoreHidden();
 }
 
 function dinoTheme() {
+  getHiddenIndex();
   if (startScreen.className != 'hidden') {
     startScreen.className = 'hidden';
   }
@@ -95,6 +121,12 @@ function dinoTheme() {
     cardList[x].className = 'card-back dino-back';
   }
   document.querySelector('#bg').className = 'dino-bg';
+  if (winScreen.classList.value.includes('hidden')) {
+    winScreen.className = 'you-win dino-win hidden';
+  } else {
+    winScreen.className = 'you-win dino-win';
+  }
+  restoreHidden();
 }
 
 
@@ -111,7 +143,12 @@ function handleClick(event) {
     secondCardClasses = secondCardClicked.previousElementSibling.className;
     gameCards.removeEventListener('click', handleClick);
     if (firstCardClasses === secondCardClasses) {
-      console.log('match');
+      hiddenClass = firstCardClicked.previousElementSibling.getAttribute('class').split(' ');
+      if (storeHiddenClasses == null) {
+        storeHiddenClasses = hiddenClass[0] + ' ';
+      }  else {
+        storeHiddenClasses += hiddenClass[0] + ' ';
+      }
       gameCards.addEventListener('click', handleClick);
       firstCardClicked = null;
       secondCardClicked = null;
@@ -119,10 +156,15 @@ function handleClick(event) {
       attempts++;
       displayStats();
       if (matches === maxMatches) {
-        youWin.classList.remove('hidden');
+        youWin.classList.replace('hidden', 'show');
+        congrats.classList.add('show');
+        congrats.classList.remove('hiddenFade');
+        container.classList.add('hiddenFade');
+        setTimeout(function () {
+          document.querySelector('.play-again-container').classList.remove('hidden');
+        }, 1500);
       }
     } else {
-      console.log('no match');
       setTimeout(function () {
         firstCardClicked.classList.remove('hidden');
         secondCardClicked.classList.remove('hidden');
@@ -133,6 +175,21 @@ function handleClick(event) {
         displayStats();
       }, 1000);
     }
+  }
+}
+
+function getHiddenIndex() {
+  for (let i = 0; i < 18; i++) {
+    if (gameCards.children[i].lastElementChild.classList.value.includes('hidden')) {
+      hiddenIndex.push(i);
+      }
+  }
+}
+
+function restoreHidden() {
+  for (let i = 0; i < hiddenIndex.length; i++) {
+    let x = hiddenIndex[i];
+    gameCards.children[hiddenIndex[i]].lastElementChild.classList.value += 'hidden';
   }
 }
 
